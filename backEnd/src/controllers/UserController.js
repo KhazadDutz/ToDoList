75ee10697d62ccf3ae85467c98435cd7d79bdec0
Mauricio Ieiri <mauricio.s.ieiri@gmail.com>
2023-05-04 00:40:19 -0300

    User controller criado

---
+++ backEnd/src/controllers/UserController.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+
+// Create a new user
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.boy;
+
+    const newUser = await User.create({ name, email, password });
+
+    res.status(201).json({ success: true, message: 'User created successfully', data: newUser });
+  } catch (error) {
+    console.error(`Error creating user: ${error}`)
+    res.status(500).json({ success: false, error: 'Failed to create user' });
+  }
+};
+
