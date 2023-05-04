a0dc6e91cf2b44c1c82066bdc514134d31bc4c85
Mauricio Ieiri <mauricio.s.ieiri@gmail.com>
2023-05-04 15:26:31 -0300

    chore: fluxo de criaçao de usuário, com validação de duplicidade no database, implementada

--- backEnd/src/controllers/UserController.js
+++ backEnd/src/controllers/UserController.js
@@ -1,20 +1,20 @@
-const User = require('../models/UserModel');
+const User = require('../services/UserService');
 
 // Create a new user
 const createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     
-    const newUser = await User.create({ name, email, password });
+    const newUser = await User.createUser({ name, email, password });
     
-    res.status(201).json({ 
+    return res.status(201).json({ 
       success: true,
       message: 'User created successfully',
       data: newUser
     });
   } catch (error) {
-    console.error(`Error creating user: ${error}`)
-    res.status(500).json({ success: false, error: 'Failed to create user' });
+    console.error(error.message)
+    return res.status(500).json({ success: false, error: error.message });
   }
 };
 
--- backEnd/src/services/UserService.js
+++ backEnd/src/services/UserService.js
@@ -2,15 +2,38 @@ const User = require('../models/UserModel');
 
 const createUser = async (user) => {
   try {
+    const { success } = await findUserByName(user.name);
+    if (success) {
+      throw new Error('User already registered');
+    }
     const createdUser = await User.create(user);
-
     return { success: true, message: 'User created successfully', user: createdUser };
   } catch (error) {
-    console.error(`Error creating user: ${error}`);
-    return { success: false, error: 'Failed to create user' };
-  }
+    console.error(`Error creating user: ${error.message}`);
+    throw new Error(`Failed to create user: ${error.message}`);
+  };
 };
 
+
+const findUserByName = async (name) => {
+  try {
+    const foundUser = await User.findOne({ where: {name} });
+    if (!foundUser) {
+      return { 
+        success: false,
+        message: 'User not found'
+      };
+    }
+    return { 
+      success: true, 
+      message: 'User found successfully', 
+      user: foundUser 
+    };
+  } catch (error) {
+    console.error(`Error finding user: ${error}`);
+    throw new Error('Error finding user')
+  };
+};
 
 module.exports = {
   createUser
